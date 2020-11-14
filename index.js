eedc339e94d35f6905cb6755415963aa92cc290e
cabe2020 <52680168+cabe2020@users.noreply.github.com>
2020-11-14 01:49:47 -0300

    first commit

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const fetch = require('node-fetch')
 
 const banned = JSON.parse(fs.readFileSync('./settings/banned.json'))
 const nsfw_ = JSON.parse(fs.readFileSync('./lib/NSFW.json'))
-const simi = JSON.parse(fs.readFileSync('./settings/simi.json'))
 
 const { 
     removeBackgroundFromImageBase64
